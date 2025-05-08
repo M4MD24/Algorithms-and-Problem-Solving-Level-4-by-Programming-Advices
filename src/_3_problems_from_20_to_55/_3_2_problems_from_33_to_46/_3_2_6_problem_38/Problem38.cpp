007e07ac527831b7296ffc74e9061541f7f89681
m4md24 <m4md24@gmail.com>
2025-05-09 02:29:01 +0300

    # Solve Problem 38

---
+++ src/_3_problems_from_20_to_55/_3_2_problems_from_33_to_46/_3_2_6_problem_38/Problem38.cpp
@@ -0,0 +1,198 @@
+#include <iomanip>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+struct Date {
+    short year;
+    short month;
+    short day;
+};
+
+bool isLeapYear(
+    const short& YEAR
+) {
+    return YEAR % 4 == 0 &&
+    (
+        YEAR % 100 != 0 ||
+        YEAR % 400 == 0
+    );
+}
+
+bool isNumber(
+    short& number
+) {
+    cin >> number;
+    if (
+        const bool VALID = !cin.fail();
+        !VALID
+    ) {
+        cin.clear();
+        cin.ignore(
+            numeric_limits<streamsize>::max(),
+            '\n'
+        );
+        return false;
+    }
+    return true;
+}
+
+bool isPositiveNumber(
+    const short& NUMBER
+) { return NUMBER > 0; }
+
+short readYear() {
+    short year;
+    do cout << "Enter Year:" << endl;
+    while (
+        !isNumber(
+            year
+        ) || !isPositiveNumber(
+            year
+        )
+    );
+    return year;
+}
+
+short readMonth() {
+    short month;
+    do cout << "Enter Month:" << endl;
+    while (
+        !isNumber(
+            month
+        ) || !isPositiveNumber(
+            month
+        ) ||
+        month > 12
+    );
+    return month;
+}
+
+short monthDays(
+    const short& YEAR,
+    const short& MONTH
+) {
+    if (MONTH == 2)
+        return isLeapYear(
+                   YEAR
+               )
+                   ? 29
+                   : 28;
+
+    switch (MONTH) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        return 31;
+    default:
+        return 30;
+    }
+}
+
+short readDay(
+    const short& YEAR,
+    const short& MONTH
+) {
+    const short MONTH_DAYS = monthDays(
+        YEAR,
+        MONTH
+    );
+    short day;
+    do cout << "Enter Day:" << endl;
+    while (
+        !isNumber(
+            day
+        ) || !isPositiveNumber(
+            day
+        ) ||
+        day > MONTH_DAYS
+    );
+    return day;
+}
+
+Date readDate() {
+    const short YEAR = readYear();
+    const short MONTH = readMonth();
+    const short DAY = readDay(
+        YEAR,
+        MONTH
+    );
+    return {
+        YEAR,
+        MONTH,
+        DAY
+    };
+}
+
+short readMonths(
+    const string& INPUT_MESSAGE
+) {
+    short months;
+    do cout << INPUT_MESSAGE << endl;
+    while (
+        !isNumber(
+            months
+        ) || !isPositiveNumber(
+            months
+        )
+    );
+    return months;
+}
+
+void previousMonth(
+    Date& date
+) {
+    if (--date.month == 0) {
+        date.month = 12;
+        date.year--;
+    }
+
+    if (
+        const short MONTH_DAYS = monthDays(
+            date.year,
+            date.month
+        );
+        date.day > MONTH_DAYS
+    )
+        date.day = MONTH_DAYS;
+}
+
+void printDate(
+    const Date& DATE,
+    const char& SEPARATOR
+) { cout << "Date: " << DATE.day << SEPARATOR << DATE.month << SEPARATOR << DATE.year; }
+
+void previousMonths(
+    short& monthCount,
+    Date& date
+) {
+    while (monthCount--)
+        previousMonth(
+            date
+        );
+}
+
+int main() {
+    Date date{
+        readDate()
+    };
+    constexpr char SEPARATOR = '-';
+
+    short monthCount = readMonths(
+        "How Many Months to Subtract?"
+    );
+
+    previousMonths(
+        monthCount,
+        date
+    );
+
+    printDate(
+        date,
+        SEPARATOR
+    );
+}
